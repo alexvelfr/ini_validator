45cf808831e0d3136e3e06326bdb6c468b0ed465
alex <kudrushow@gmail.com>
2019-05-02 23:21:29 +0300

    reformat to python2

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ from configparser import RawConfigParser, MissingSectionHeaderError, ParsingErro
 
 def validate(file):
     try:
-        with open(file, 'r') as f:
-            data = f.read()
-    except FileNotFoundError:
-        print('File not found')
+        f = open(file, 'r')
+        data = f.read()
+    except:
+        print 'File not found'
         exit(1)
     conf_parser = RawConfigParser()
     try:
-        conf_parser.read_string(data)
+        conf_parser.read_string(unicode(data))
     except (MissingSectionHeaderError, ParsingError):
-        print('Parse error. File not valid')
+        print 'Parse error. File not valid'
         exit(1)
     exit(0)
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print(f'Usage: python3 {sys.argv[0]} filename')
+        print 'Usage: python3 %s filename' % sys.argv[0]
         exit(1)
     validate(sys.argv[1])
