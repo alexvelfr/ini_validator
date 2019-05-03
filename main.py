f28aed11052bbf23525f58f895b74bd6d3c68355
alex <kudrushow@gmail.com>
2019-05-03 22:43:55 +0300

    add another validation

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 import sys
-from configparser import RawConfigParser, MissingSectionHeaderError, ParsingError, DuplicateOptionError, DuplicateSectionError
 
 
 def validate(file):
-    try:
-        f = open(file, 'r')
-        data = f.read()
-        f.close()
-    except:
-        print 'File not found'
-        exit(1)
-    conf_parser = RawConfigParser()
-    try:
-        conf_parser.read_string(unicode(data))
-    except (MissingSectionHeaderError, ParsingError,DuplicateOptionError, DuplicateSectionError):
-        print 'Parse error. File not valid'
-        exit(1)
-    exit(0)
-
-
-def validate2(file):
     excepted_symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '/', '?', '\\', '"', '\'']
     try:
         f = open(file, 'r')
@@ -50,7 +32,6 @@ def validate2(file):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print 'Usage: python3 %s filename' % sys.argv[0]
+        print 'Usage: python %s filename' % sys.argv[0]
         exit(1)
-    # validate(sys.argv[1])
-    validate2(sys.argv[1])
+    validate(sys.argv[1])
