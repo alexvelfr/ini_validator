75aa1dbc0720ca7c1d61a425c79acf182d10d93e
alex <kudrushow@gmail.com>
2019-05-03 10:54:09 +0300

    add another validation

--- main.py
+++ main.py
@@ -19,8 +19,35 @@ def validate(file):
     exit(0)
 
 
+def validate2(file):
+    excepted_symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '/', '?', '\\', '"', '\'']
+    f = open(file, 'r')
+    valid = True
+    for line in f.readlines():
+        arr_line = line.strip('\n, ').split('=')
+        if len(arr_line) == 1:
+            if not (arr_line[0].startswith(u'[') and arr_line[0].endswith(u']')):
+                valid = False
+                break
+        elif len(arr_line) > 1:
+            for sm in excepted_symbols:
+                if sm in arr_line[0]:
+                    valid = False
+                    break
+        else:
+            valid = False
+            break
+    f.close()
+
+    if valid:
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print 'Usage: python3 %s filename' % sys.argv[0]
         exit(1)
-    validate(sys.argv[1])
+    # validate(sys.argv[1])
+    validate2(sys.argv[1])
